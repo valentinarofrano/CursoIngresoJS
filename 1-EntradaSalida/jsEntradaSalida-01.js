0a56ca8876a2dcaedb64948178ead6cd9ced10aa
valentinarofrano <78183045+valentinarofrano@users.noreply.github.com>
2021-02-01 22:22:18 -0300

    Add files via upload
    primer commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	nombreIngresado = document.getElementById ("txtIDnombre").value
 }
 
